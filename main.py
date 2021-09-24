234093faf2dbd2c7f1a004ce1142967fb3be3889
Abdel-RahmanSaied <abdo.am169@gmail.com>
2021-09-24 20:18:37 +0200

    i create the part of fill table and check if period available or no

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-# math = {'lec':
-#             {'Day':"D1" ,
-#              "period":{"S_P":1 , "E_P":3}
-#              }
-#         }
+import json
 
 material = {
 'math' : { "Group":"G1" ,
@@ -14,8 +10,8 @@ material = {
         "section": dict(Day="D3" , period={"S_P":3 , "E_P":4}) } ,
 
 'english' :  { "Group":"G1" ,
-        'lecture':dict(Day="D1", period=[4,5]) ,
-        "section": dict(Day="D3" , period=[1,2]) }
+        'lecture':dict(Day="D1", period={"S_P":7 , "E_P":8}) ,
+        "section": dict(Day="D3" , period={"S_P":5 , "E_P":6}) }
 
 }
 
@@ -32,19 +28,43 @@ table = {"D1":{"p1": '' , "p2":'' , "p3":'' , "p4":'' , "p5":'' , "p6":'' , 'p7'
          "D4":{"p1": '' , "p2":'' , "p3":'' , "p4":'' , "p5":'' , "p6":'' , 'p7':'' , "p8":''},
          "D5":{"p1": '' , "p2":'' , "p3":'' , "p4":'' , "p5":'' , "p6":'' , 'p7':'' , "p8":''}}
 
-for i in material.keys() :
+
+for subject in material.keys() :
         #check day if found
-        lec_day =material[i]['lecture']['Day']
-        #check period content for lecture
-        lec_s_p = periods[material[i]['lecture']['period']["S_P"]-1]
-        lec_e_p = periods[material[i]['lecture']['period']["E_P"]-1]
-        if len(table[lec_s_p:lec_e_p]) == 0 :
-            table[lec_s_p:lec_e_p] = i
-        #check day if found
-        lec_day =material[i]['section']['Day']
-        #check period content for Section
-        sec_s_p = periods[i["section"]['period']["S_P"]-1]
-        sec_e_p = periods[i["section"]['period']["E_P"]-1]
-        if len(table[sec_s_p:sec_e_p]) == 0 :
-            table[sec_s_p:sec_e_p] = i
-print(table)
+        lec_day =material[subject]['lecture']['Day']
+        sec_day = material[subject]['section']['Day']
+        #period content for lecture
+        lec_s_p = periods[material[subject]['lecture']['period']["S_P"]-1]
+        lec_e_p = periods[material[subject]['lecture']['period']["E_P"]-1]
+        #  period content for Section
+        sec_s_p = periods[material[subject]["section"]['period']["S_P"] - 1]
+        sec_e_p = periods[material[subject]["section"]['period']["E_P"] - 1]
+
+        for lec_index in range(material[subject]['lecture']['period']["S_P"]-1 , material[subject]['lecture']['period']["E_P"] , 1) :
+            lec_checker = []
+            if len(table[lec_day][periods[lec_index]]) == 0 :
+                lec_checker.append(True)
+                try :
+                    if lec_checker[0] == True and lec_checker[1] == True and lec_checker[2] == True :
+                        continue
+
+                except :
+                    pass
+                finally:
+                    table[lec_day][periods[lec_index]] = subject
+
+                    #############################################
+        for sec_index in range(material[subject]['section']['period']["S_P"]-1 , material[subject]['section']['period']["E_P"] , 1) :
+            sec_checker = []
+            if len(table[sec_day][periods[sec_index]]) == 0 :
+                sec_checker.append(True)
+                try :
+                    if sec_checker[0] == True and sec_checker[1] == True and sec_checker[2] == True :
+                        continue
+
+                except :
+                    pass
+                finally:
+                    table[sec_day][periods[sec_index]] = subject
+
+print(json.dumps(table , indent=4))
