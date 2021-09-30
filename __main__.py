3647d2c64e82169db4ba47a9c84a78b5f49fe4fe
Abdel-RahmanSaied <abdo.am169@gmail.com>
2021-09-30 05:07:20 +0200

    all functions done

--- __main__.py
+++ __main__.py
@@ -2,14 +2,14 @@ from final_subject_manger import Subject_Manger
 import json
 # take group of eatch of subject and pass it to lecture checker
 
-with open(r"DB/materials.json") as material_db:
+with open(r"DB/teeest.json") as material_db:
     materials = json.load(material_db)
-
 
 sub_material = Subject_Manger().subject_generator(materials)
 
 print(len(sub_material))
-
+all_tables = []
+success_counter = 0
 for mat_counter in range(len(sub_material)):
     new_mat = {}
     for mat_counter in list(sub_material[mat_counter]):
@@ -21,8 +21,10 @@ for mat_counter in range(len(sub_material)):
         sec_check = Subject_Manger().section_checker(new_mat)
         if sec_check == None :
             table_fill = Subject_Manger().table_viewer(new_mat)
-            print(f" table = {table_fill}")
+            success_counter +=1
+            all_tables.append(table_fill)
+#print(all_tables)
+print(success_counter)
 
 
 
-    # print(lec_check)
---
+++ compile_ui.bat
@@ -0,0 +1,7 @@
+pyuic5 forms/add_subject_view.ui -o views/login_view.py
+pyuic5 forms/main_view.ui -o views/main_view.py
+pyuic5 forms/table_view.ui -o views/predict_view.py
+
+
+
+pyrcc5 forms/app_resources.qrc -o app_resources_rc.py
